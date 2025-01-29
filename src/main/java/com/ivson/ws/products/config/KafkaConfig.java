ed66f91d79dd11011e118707aba16dc19e2d5290
isantos <ivson.santos@ottogroup.com>
2025-01-29 12:06:02 +0100

    Configurer para criar um topico no kafka

---
+++ src/main/java/com/ivson/ws/products/config/KafkaConfig.java
@@ -0,0 +1,30 @@
+package com.ivson.ws.products.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+
+    private Map<String, String> configs = Map.of(
+        // 1 replica precisa estar sincronizad com sucesso antes de confirmar a escrita
+        // quanto mais acima de 1 mais lenta fica, mas maior segurnacao de backup
+        "min.insync.replicas", "1"
+    );
+
+    @Bean   // poe este metodo no contexto do spring
+    public NewTopic createTopic() {
+        return TopicBuilder.name("product-created-evetns-topic")
+                // ate 3 microservicos podem consumir este topic
+                .partitions(3)
+                // 3 (1) replicas para garantir a disponibilidade, 1 copia em cada um dos 3 brokers
+                .replicas(1)
+                .configs(configs)
+                .build();
+    }
+
+}
